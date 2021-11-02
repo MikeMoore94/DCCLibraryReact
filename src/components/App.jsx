391d9cd5a47342aa86d3378ba99669ee55e52fbf
Micheal Moore <michealmoore@Micheals-MacBook-Air.local>
2021-11-02 16:35:07 -0400

    split book viewing logic into its own component file bookviewer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import './App.css'
 import TitleBar from './TitleBar/TitleBar'
+import BookViewer from './BookViewer/BookViewer'
 class App extends Component{
     constructor(props){
         super(props)
@@ -16,7 +17,7 @@ class App extends Component{
     }
 
 
-    gotoNextBook = () => {
+    goToNextBook = () => {
         let tempBookNumber = this.state.bookNumber;
         tempBookNumber++;
         if(tempBookNumber === this.books.length){
@@ -26,27 +27,27 @@ class App extends Component{
             bookNumber: tempBookNumber
          });
     }
+    goToPreviousBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        
+          if(tempBookNumber < 0){
+            tempBookNumber = this.books.length -1;
+        }
 
+         this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
 
 
     render(){
         return(
          <div className= 'container-fluid'>
              <TitleBar />
-             <div className = "row">
-             <div className='col-md-4'>
-                 {/*Button here to move to previous book view*/}
-             </div>
-             <div className='col-md-4'>
-                 {/*Display book with cover here*/}
-                 <h1>{this.books[this.state.bookNumber].title}</h1>
-                 <h4>{this.books[this.state.bookNumber].author}</h4>
-            </div>
-               <div className='col-md-4'>
-                {/*Button here to move to the next book viewed*/}
-                <button onClick={this.gotoNextBook}>Next Book</button>
-               </div>
-             </div>
+             
+             <BookViewer book={this.books[this.state.bookNumber]} nextBook={this.goToNextBook} previousBook={this.goToPreviousBook} />
+                
             
                
             </div>
